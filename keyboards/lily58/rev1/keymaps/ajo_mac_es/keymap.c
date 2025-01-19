8271c427484969ef6a266b5c061c91eb88f42a72
Miguel Angel Ajo Pelayo <miguelangel@ajo.es>
2025-01-19 20:34:14 +0100

    Add screen lock for double ESC press

--- keyboards/lily58/rev1/keymaps/ajo_mac_es/keymap.c
+++ keyboards/lily58/rev1/keymaps/ajo_mac_es/keymap.c
@@ -18,8 +18,9 @@ enum layer_number {
 // tap dance is used to program keys that have different functions when tapped once or twice (quickly)
 // tap dance examples : https://docs.qmk.fm/features/tap_dance#simple-example
 enum {
-    TD_BRL_COPY,
-    TD_BRR_PASTE,
+    TD_BRL_COPY, // tap once for [, twice for copy
+    TD_BRR_PASTE, // tap once for ], twice for paste
+    TD_ESC_LOCK, // tap once for ESC, twice for lock screen on Mac OS
 };
 
 // Tap Dance definitions
@@ -27,13 +28,14 @@ tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_BRL_COPY] = ACTION_TAP_DANCE_DOUBLE(ES_LBRC, LGUI(KC_C)),
     [TD_BRR_PASTE] = ACTION_TAP_DANCE_DOUBLE(ES_RBRC, LGUI(KC_V)),
+    [TD_ESC_LOCK] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, LCTL(LGUI(KC_Q))),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |  s 7  |   8  |   9  |   0  |  `   |
+ * |ESC+L |   1  |   2  |   3  |   4  |   5  |                    |   6  |  s 7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   q  |   w  |   e  |   r  |   t  |                    |   y  |   u  |   i  |   o  |   p  |  +   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -47,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,      KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ES_GRV,
-  KC_TAB,      KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ES_PLUS,
-  KC_LCTL,     KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,     KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TD(TD_BRL_COPY),   TD(TD_BRR_PASTE),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+  TD(TD_ESC_LOCK),KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ES_GRV,
+  KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ES_PLUS,
+  KC_LCTL,        KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TD(TD_BRL_COPY),   TD(TD_BRR_PASTE),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                          KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,                    KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
